61ce7d0386c04877438381f90740cfdf98ccf72c
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-05-26 17:36:34 -0400

    restful: PUT /addUser method added. It saves one or more new user(s) but cancels the whole transaction if at least one entry already exists, i.e. refuses to update existing entries.

--- restful/server.js
+++ restful/server.js
@@ -57,8 +57,47 @@ app.delete('/deleteUser/:id', (req, resp) => {
     });
 });
 
+app.put('/addUser', (req, resp) => {
+
+    var existingData={};
+    var entriesControl = {anyExistingEntry : false, names : [] } ;
+    var finalOutput;
+    
+    fs.readFile(`${__dirname}/${FILE_NAME}`, 'utf8', (err, dataFromFile) => {
+        
+        existingData = JSON.parse( dataFromFile );
+        
+        for(propertyName in req.body) {
+            if(existingData[ propertyName ] != null || existingData[ propertyName ] != undefined ) {
+                entriesControl.anyExistingEntry = true;
+                entriesControl.names.push( propertyName );
+            }
+            else
+                existingData[ propertyName ] = req.body[ propertyName ];
+        } 
+        
+        if (entriesControl.anyExistingEntry) {
+            finalOutput =
+            `The following entry or entries already exists: ${entriesControl.names.join(', ')}. The whole object will be ignored.
+            `+
+           `Verify your payload or method, for PUT( /addUser ) is intended only for saving new entries. If your purpose is to update an existing entry, try instead POST( /addUser )`;
+            
+            resp.end( finalOutput );
+        }
+        else {
+            finalOutput = existingData;
+            var writerStream = fs.createWriteStream(FILE_NAME);
+            writerStream.write(JSON.stringify(finalOutput), {flags:'w',encoding:'utf8', fd:null, mode:0o666, autoclose:true, start:0} );
+            writerStream.end(); 
+            resp.end( JSON.stringify(finalOutput) );
+        }
+        console.log( finalOutput );
+
+    });
+
+});
 
 var server = app.listen(PORT, () => {
     var host = server.address().address;    
-    console.log("Example app listening at http://%s:%s", host, PORT);
+    console.log("RESTful app listening at http://%s:%s", host, PORT);
 });
