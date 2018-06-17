c7aee0fd7b556e718ac2c0e9074b3b912231d443
Roberto M @ASUS Laptop <errrzarrr@gmail.com>
2018-06-17 10:13:39 -0400

    how to create a index in mongo CLI & mongod.cfg file

--- db/nosql/db_script.sh
+++ db/nosql/db_script.sh
@@ -2,13 +2,21 @@
 
 var key = {name:1};
 var options = {         
-	"unique" : 1
+	"unique" : true
 	,"name":  "nameIsUniqueAndCaseSensitive"
 	,"collation" : {                 
 		"locale" : "en" 
 		,"strength" : 2
 		,"caseLevel" : true         
-	}      
+	}
 };
+db.products.dropIndex(options.name);
 db.products.createIndex(key, options);
 
+var key = {categories:1};
+var options = {         
+	"unique" : false 
+	,"name":  "categoriesIndex"
+};
+db.products.dropIndex(options.name);
+db.products.createIndex(key, options);
