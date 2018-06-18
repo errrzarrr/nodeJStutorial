bca80dbcecafda5cdb1291d860724d4cee1586d8
Roberto <errrzarrr@Gmail.com>
2018-06-18 14:50:10 -0400

    mongo repli, dump
    replication and db dump on db_script.sh

--- db/nosql/db_script.sh
+++ db/nosql/db_script.sh
@@ -1,4 +1,4 @@
-# script for executing in mongo client console.
+# scripts for executing in mongo client console.
 
 var key = {name:1};
 var options = {         
@@ -20,3 +20,26 @@ var options = {
 };
 db.products.dropIndex(options.name);
 db.products.createIndex(key, options);
+
+
+# replication: 
+mongod --port 27017 --dbpath "c:\mongo_data\db" --replSet rs0.
+
+# on mongo client:
+rs.isMaster( );
+rs.status();
+rs.conf();
+
+# backup
+# FULL db dump, i.e all collections from all dbs. Dumps it in current dir.
+mongodump 
+# or mongodump --host localhost --port 27017
+
+# dumping a specific db
+mongodump --host localhost --port 27017 --db mydb
+
+# dumping a specific collection from mydb
+mongodump --host localhost --port 27017  --collection products  --db mydb
+
+# restore
+mongorestore
